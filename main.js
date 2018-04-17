5c39173ac111f9744a028727345e6390d864db9a
MatthyPlayz <34276633+MatthyPlayz@users.noreply.github.com>
2018-04-17 16:30:09 -0400

    Create main.js

---
+++ main.js
@@ -0,0 +1,6 @@
+function downloadGitHub() {
+  window.open("https://github.com/MatthyPlayz/Serh")
+}
+function downloadDirect() {
+  window.open("https://github.com/MatthyPlayz/Serh/archive/master.zip")
+}
